e658ab584fad28dff7392b0af7ce82bb1b4e7347
Iwantpizza <lxsguaiquian@gmail.com>
2022-12-12 18:37:35 -0300

    archivo js

---
+++ main.js
@@ -0,0 +1,42 @@
+
+
+const inputOne = document.getElementById('op1')
+
+const inputTwo = document.getElementById('operador')
+
+const inputThree = document.getElementById('op2')
+
+const inputButton = document.getElementById('button')
+
+const resultadoCuenta = document.getElementById('resultado')
+
+
+// agregue un evento listener, cuando el usuario haga click en el btn se ejecutara la funcion
+inputButton.addEventListener('click', calcular)
+
+function calcular(){
+    const operador = inputTwo.value
+    const op1 = parseFloat(inputOne.value)
+    const op2 = parseFloat(inputThree.value)
+    if((operador == '+' || operador == '-' || operador == '*' || operador == '/' )&& !isNaN(op1) && !isNaN(op2)){
+        let result
+        switch(operador){
+            case '+':
+                result = op1 + op2
+                break;
+            case '-':
+                result = op1 - op2
+                break;
+            case '/':
+                result = op1 / op2
+                break;
+            case '*':
+                result = op1 * op2
+                break;
+        }
+        resultadoCuenta.innerText = '= ' + result
+
+    }else{
+        resultadoCuenta.innerText = 'Operacion No Valida'
+    }
+}
